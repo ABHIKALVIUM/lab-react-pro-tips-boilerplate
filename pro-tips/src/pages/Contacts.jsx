b658316ca4badf473eb98f1a5ed98853f64d9d52
ABHIKALVIUM <144119494+ABHIKALVIUM@users.noreply.github.com>
2024-01-17 13:51:29 +0530

    new

---
+++ pro-tips/src/pages/Contacts.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const Contact=()=>{
+    return(
+        <h1 style={{textAlign:"center", position:"absolute",top:"100px",color:"black"}}>Contact</h1>
+    )
+}
+export default Contact
---
+++ pro-tips/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Home=()=>{
+    return(
+    <h1 style={{textAlign:"center", position:"absolute",top:"100px",color:"black"}}>Home</h1>
+    )
+}
+export default Home
